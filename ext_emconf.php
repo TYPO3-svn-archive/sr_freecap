fd72418a921a46db92a077e7e95566a92bdc5793
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2010-04-16 15:03:04 +0000

    Require TYPO3 4.3
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_freecap/trunk@32275 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -36,7 +36,7 @@ $EM_CONF[$_EXTKEY] = array(
 		'depends' => array(
 			'cms' => '',
 			'php' => '5.0.0-0.0.0',
-			'typo3' => '4.0.0-0.0.0',
+			'typo3' => '4.3.0-0.0.0',
 		),
 		'conflicts' => array(
 		),
