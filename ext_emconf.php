4a769351660504587f2a430e7b7a471d4f335deb
stan <stan@735d13b6-9817-0410-8766-e36946ffe9aa>
2015-10-12 22:45:19 +0000

    Add composer.json
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/sr_freecap/trunk@89886 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,48 +1,27 @@
 <?php
-
-/***************************************************************
- * Extension Manager/Repository config file for ext "sr_freecap".
+/*
+ * Extension Manager configuration file for ext "sr_freecap".
  *
- * Auto generated 21-02-2014 21:01
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
-$EM_CONF[$_EXTKEY] = array (
+ */
+$EM_CONF[$_EXTKEY] = array(
 	'title' => 'freeCap CAPTCHA',
 	'description' => 'A TYPO3 integration of freeCap CAPTCHA.',
 	'category' => 'plugin',
-	'shy' => 0,
 	'version' => '2.2.0',
-	'priority' => '',
-	'loadOrder' => '',
-	'module' => '',
 	'state' => 'stable',
 	'uploadfolder' => 1,
 	'createDirs' => '',
-	'modify_tables' => '',
 	'clearcacheonload' => 0,
-	'lockType' => '',
 	'author' => 'Stanislas Rolland',
 	'author_email' => 'typo3(arobas)sjbr.ca',
 	'author_company' => 'SJBR',
-	'CGLcompliance' => '',
-	'CGLcompliance_note' => '',
 	'constraints' => 
-	array (
-		'depends' => 
-		array (
+	array(
+		'depends' => array(
 			'php' => '5.3.0-0.0.0',
-			'typo3' => '6.2.0-7.99.99',
+			'typo3' => '6.2.0-7.99.99'
 		),
-		'conflicts' => 
-		array (
-		),
-		'suggests' => 
-		array (
-		),
-	),
-	'_md5_values_when_last_written' => '',
+		'conflicts' => array(),
+		'suggests' => array()
+	)
 );
